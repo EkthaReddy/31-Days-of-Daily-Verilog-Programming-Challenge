ebac40a1f3672ef2e3b6c616c4dbcde2ccbf63bb
Ektha Reddy <152515939+EkthaReddy@users.noreply.github.com>
2024-06-02 13:33:17 +0530

    Create Design-For-Or-Gate.v

---
+++ Day 2 - Or Gate/Design-For-Or-Gate.v
@@ -0,0 +1,26 @@
+// Data Flow Modeling For OR Gate
+module Or_Gate_DF(
+input a,
+input b,
+output y 
+);
+    assign y=a|b;
+endmodule
+
+-------------------------------------------------------------------
+//Behavioral Modeling For OR Gate
+module Or_Gate_BM(a,b,y);
+    input a,b;
+    output reg y;
+    always@(*)
+        y=a|b;
+endmodule
+
+-------------------------------------------------------------------
+//Structural Modeling For OR Gate
+module Or_Gate_SM(
+input a,b,
+output y
+    );
+    or o(y,a,b);
+endmodule
