4c8e874a413d9e6c6fe837c3cd8c7b68a160a377
Ektha Reddy <152515939+EkthaReddy@users.noreply.github.com>
2024-06-02 14:23:22 +0530

    Update Not-Gate-Design.v

--- Day 3 - Not Gate/Not-Gate-Design.v
+++ Day 3 - Not Gate/Not-Gate-Design.v
@@ -1,11 +1,15 @@
+// Three Types of modelling in NOT Gate
+=======================================
 
+// Data Flow Modeling For NOT Gate
 module Not_Gate_DF(
 input a,
 output y);
     assign y=~a;
 endmodule
 
-
+------------------------------------------------------
+//Behavioral Modeling For NOT Gate
 module Or_Gate_BM(
 input a, 
 output y
@@ -13,7 +17,8 @@ output y
     not n(y,a);
 endmodule
 
-
+------------------------------------------------------
+//Structural Modeling For NOT Gate
 module Or_Gate_SM(
 input a,
 output reg y
