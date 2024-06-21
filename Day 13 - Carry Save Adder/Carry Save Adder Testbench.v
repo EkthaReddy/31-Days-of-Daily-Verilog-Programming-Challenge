48f58a7869c1e73da2bde22d58d388de3fee96ae
Ektha Reddy <152515939+EkthaReddy@users.noreply.github.com>
2024-06-21 20:26:54 +0530

    Update Carry Save Adder Testbench.v

--- Day 13 - Carry Save Adder/Carry Save Adder Testbench.v
+++ Day 13 - Carry Save Adder/Carry Save Adder Testbench.v
@@ -1,1 +1,26 @@
+//Testbench
+
+module Carry_Save_Adder_tb;
+reg [3:0]a, b, cin;
+wire [3:0]sum;
+wire carry;
+
+Carry_Save_Adder dut(.a(a), .b(b), .cin_csa(cin), .din(cin), .sum(sum), .carry(carry));
+
+initial begin
+$dumpfile("dump.vcd");
+$dumpvars(0);
+
+a<=1'b0;
+b<=1'b0;
+cin<=1'b0;
+
+#10 $finish();
+end
+
+always #5 a<=~a;
+always #2 b<=~b;
+always #7 cin<=~cin;
+
+endmodule
 
