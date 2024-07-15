f3308b2f9547565ce7ff3dc9e7b23a5b06baf651
Ektha Reddy <152515939+EkthaReddy@users.noreply.github.com>
2024-07-15 19:21:35 +0530

    Create Up Counter Testbench.v

---
+++ Day 26 - Up Counter/Up Counter Testbench.v
@@ -0,0 +1,23 @@
+//Testbench
+
+module Up_Counter_tb;
+reg [3:0]data, rst, clk;
+wire [3:0]out;
+
+Up_Counter dut(data, rst, clk, out);
+
+initial begin
+$dumpfile("dump.vcd");
+$dumpvars(0);
+
+data<=1'b0;
+clk<=1'b0;
+rst<=1'b0; 
+
+#20 $finish();
+end
+
+always #3 data<=~data;
+always #0.9 clk<=~clk;
+always #1.5 rst<=~rst;
+endmodule
