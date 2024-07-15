1006c431e7bb60e996ed28b40ec5304a62b77644
Ektha Reddy <152515939+EkthaReddy@users.noreply.github.com>
2024-07-15 19:32:45 +0530

    Create Asynchronous Counter Testbench.v

---
+++ Day 29 - Asynchronous Counter/Asynchronous Counter Testbench.v
@@ -0,0 +1,24 @@
+//Testbench
+
+module tb;
+reg clk;
+reg rst;
+wire [3:0]q;
+
+ripplecounter dut(clk,rst,q);
+initial
+clk = 0;
+always
+#5 clk = ~clk;
+
+initial
+begin
+rst = 1;
+#15 rst = 0;
+#180 rst = 1;
+#10 rst = 1;
+#20 $finish;
+end
+initial
+$monitor("time=%g,rst=%b,clk=%b,q=%d",$time,rst,clk,q);
+endmodule
