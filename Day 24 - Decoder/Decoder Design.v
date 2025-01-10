63a132094f7ecfb73fc1f6169f6f5df19007bc82
Ektha Reddy <152515939+EkthaReddy@users.noreply.github.com>
2025-01-10 09:09:19 +0530

    Update Decoder Design.v

--- Day 24 - Decoder/Decoder Design.v
+++ Day 24 - Decoder/Decoder Design.v
@@ -6,11 +6,12 @@ module Decoder(
 );
 
 always @* begin
-    case(en)
+    case({in1, in2})
         2'b00: y = 4'b0001;
         2'b01: y = 4'b0010;
         2'b10: y = 4'b0100;
         2'b11: y = 4'b1000;
+        default: y = 4'b0000;
     endcase
 end
 
